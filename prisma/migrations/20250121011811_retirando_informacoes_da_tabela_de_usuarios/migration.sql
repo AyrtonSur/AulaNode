4a9cc31a6dc106d720353b2494fac82d48f06c71
Gabriel Camara <galvares439@gmail.com>
2025-01-20 22:30:56 -0300

    Aula 2

---
+++ prisma/migrations/20250121011811_retirando_informacoes_da_tabela_de_usuarios/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `birthdate` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `cpf` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `firstname` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `lastname` on the `users` table. All the data in the column will be lost.
+  - Added the required column `name` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+INSERT INTO "new_users" ("email", "id", "password", "username") SELECT "email", "id", "password", "username" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;

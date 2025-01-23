8828e55e937132b51f322c6e575bf7d8b0011bde
Gabriel Camara <galvares439@gmail.com>
2025-01-22 21:26:26 -0300

    Aula 8 - Authenticate Controller e Use Case

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,27 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
+import { AuthenticateUseCase } from "@/use-cases/authenticate-use-case"
+import { FastifyRequest, FastifyReply } from "fastify"
+import { z } from "zod"
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+     const authenticateBodySchema = z.object({        
+          email: z.string().email(),        
+          password: z.string().min(6)
+     })
+
+     const { email, password } = authenticateBodySchema.parse(request.body)
+
+     try {
+          const prismaUsersRepository = new PrismaUsersRepository()
+          const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository)
+
+          await authenticateUseCase.execute({
+               email,
+               password
+          })
+     } catch (err) {
+          
+     }
+
+     return reply.status(200).send({ message: 'Usuário autenticado com sucesso' })
+}

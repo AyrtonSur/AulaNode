09022f39e38f8e2b388a11bb6779eb5bfefd6115
Gabriel Camara <galvares439@gmail.com>
2025-01-24 19:06:28 -0300

    Aula 1 - feita

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,32 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { z } from 'zod'
 
 export const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/', async (request, reply) => {
-     return { message: 'Hello, World!' };
-});
+app.get('/hello', (request, reply) => {
+     return { message: 'Hello Word'}
+})
+
+app.post('/', async (request, reply) => {
+     const registerBodySchema = z.object({
+          name: z.string(),
+          username: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6)
+     })
+
+     const { name, username, email, password } = registerBodySchema.parse(request.body)
+
+     await prisma.user.create({
+          data: {
+               name,
+               username,
+               email,
+               password
+          }
+     })
+
+     return reply.status(201).send('Usuário criado com sucesso')
+})

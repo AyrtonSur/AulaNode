5817e8d26dd4b2cf3756f1e12e1d171a22d6f9f1
Gabriel Camara <galvares439@gmail.com>
2025-01-27 15:08:32 -0300

    Aula 7 - feita

--- src/use-cases/register-use-case.ts
+++ src/use-cases/register-use-case.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from "@/repositories/users-repository"
 import { hash } from "bcryptjs"
+import { UserAlreadyExists } from "./errors/user-already-exists"
 
 interface RegisterUseCaseRequest {
      name: string
@@ -15,7 +16,7 @@ export class RegisterUseCase {
           const userWithSameEmail = await this.usersRepository.findByEmail(email)
      
           if (userWithSameEmail) {
-               throw new Error()
+               throw new UserAlreadyExists()
           }
      
           const password_hash = await hash(password, 6)

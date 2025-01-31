762bd9754df9dfea9d85471fa87620170aaad8a1
Gabriel Camara <galvares439@gmail.com>
2025-01-31 18:33:28 -0300

    Aula 14c - Profle Controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,12 +4,15 @@ import { authenticate } from "./authenticate";
 import { get } from "./get";
 import { deleteUser } from "./delete";
 import { changePassword } from "./change-password";
+import { profile } from "./profile";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
 
 export async function userRoutes(app: FastifyInstance) {
      app.post('/users', register)
      app.post('/authenticate', authenticate)
 
      app.get('/users/:userId', get)
+     app.get('/profile', { onRequest: [verifyJWT] }, profile)
      
      app.patch('/users/:userId', changePassword)
      

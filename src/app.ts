0ad75ecbcd8766bd02bb581fa8077cd676c38002
AyrtonSur <ayrtonsurica@gmail.com>
2025-03-19 01:53:00 -0300

    refactor: consertando alguns detalhes de configuração

--- src/app.ts
+++ src/app.ts
@@ -40,5 +40,8 @@ app.setErrorHandler((error, request, reply) => {
       .status(400)
       .send({ message: 'Validation error', issues: error.format() })
   }
+
+  console.log(error)
+
   return reply.status(500).send({ message: 'Internal server error.' })
 })
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import { app } from './app'
-import { env } from './env'
+import { env } from '@/env'
 
-app
-  .listen({
+app.listen(
+  {
     host: '0.0.0.0',
     port: env.PORT,
-  })
-  .then(() => {
-    console.log('HTTP Server Running in http://localhost:3000')
-  })
+  },
+  (err, address) => {
+    if (err !== null) {
+      console.log(err)
+      app.log.error(err)
+      process.exit(1)
+    }
+    console.log(
+      `🚀 Server running! \nServer listening at ${address}\n\nCtrl+C to Stop`,
+    )
+  },
+)
